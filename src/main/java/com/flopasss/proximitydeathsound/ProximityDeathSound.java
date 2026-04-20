d15c3220387c8efd08677b538968cfe986383360
florishalleriet <florispshalleriet@gmail.com>
2026-04-20 09:01:30 +0200

    feat: load the Config and Command classes on start up

--- src/main/java/com/flopasss/proximitydeathsound/ProximityDeathSound.java
+++ src/main/java/com/flopasss/proximitydeathsound/ProximityDeathSound.java
@@ -1,9 +1,13 @@
 package com.flopasss.proximitydeathsound;
 
-import net.fabricmc.api.ModInitializer;
+import com.flopasss.proximitydeathsound.config.ProximityDeathSoundConfig;
+import com.flopasss.proximitydeathsound.command.ProximityDeathSoundCommand;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class ProximityDeathSound implements ModInitializer {
 	public static final String MOD_ID = "proximity-death-sound";
@@ -12,6 +16,7 @@ public class ProximityDeathSound implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static ProximityDeathSoundConfig CONFIG;
 
 	@Override
 	public void onInitialize() {
@@ -19,6 +24,14 @@ public class ProximityDeathSound implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		// Load the config on startup
+		CONFIG = ProximityDeathSoundConfig.load();
+
+		// Register the command
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			ProximityDeathSoundCommand.register(dispatcher);
+		});
+
+		LOGGER.info("Flopasss Proximity Death Sound initialized");
 	}
 }
